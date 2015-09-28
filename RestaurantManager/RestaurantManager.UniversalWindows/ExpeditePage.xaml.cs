d35615cdc40b71a3ab06014652e7a305871d3f5c
Esteban <ebenito@outlook.com>
2015-09-28 19:36:29 +0200

    Finish

--- RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace RestaurantManager
+namespace RestaurantManager.UniversalWindows
 {
     /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class ExpeditePage : Page
     {
-        public MainPage()
+        public ExpeditePage()
         {
             this.InitializeComponent();
         }
